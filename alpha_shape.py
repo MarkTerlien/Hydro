83a4d4c1842b25d822c563ba2f5dddb23a3c7924
Mark Terlien <m.terlien@has.nl>
2021-05-05 20:21:53 +0200

    alpha shape

--- alpha_shape.py
+++ alpha_shape.py
@@ -26,8 +26,34 @@ while row :
     row = fIn.readline()
 fIn.close()
 
-alpha_shape = alphashape.alphashape(points_2d, 2.25)
+print("Read file")
+points_2d = []
+fIn = open(r'data\H12046_1m_MLLW_1of1.xyz','r')
+row = fIn.readline()
+i = 0
+j = 0
+while row :
+    x = float(row.split(',')[0])
+    y = float(row.split(',')[1])
+    if i % 25 == 0: 
+        coordinate = []
+        coordinate.append(x)
+        coordinate.append(y)
+        points_2d.append(coordinate)
+        j = j + 1
+    i = i + 1
+    row = fIn.readline()
+fIn.close()
+print(str(i) + " rows in file")
+print(str(j) + " rows for hull")
 
+print("Calculate hull")
+alpha_shape = alphashape.alphashape(points_2d, 0.1)
+fOut = open('data/wkt.txt','w')
+fOut.write(alpha_shape.wkt)
+fOut.close()
+
+print("Plot points and hulls")
 fig, ax = plt.subplots()
 ax.scatter(*zip(*points_2d))
 ax.add_patch(PolygonPatch(alpha_shape, alpha=0.2))
